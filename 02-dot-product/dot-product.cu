cc90429bbc375d736e994d1e8220f755ccf80f36
James Osburn <78386594+JamesOzzyburn@users.noreply.github.com>
2026-01-13 20:20:17 -0700

    Implement CUDA kernel for vector dot product

---
+++ 02-dot-product/dot-product.cu
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <vector>
+#include <ranges>
+
+__global__ void vecDotProduct(float *a, float *b, float *c, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        c[i] = a[i] * b[i];
+    }
+}
+
+int main() {
+    int n {1000000};
+    size_t bytes {n * sizeof(float)};
+
+    std::vector<float> h_a(n), h_b(n), h_c(n);
+
+    for (const int i : std::views::iota(0, n)) {
+        h_a[i] = i;
+        h_b[i] = i * 2;
+    }
+
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+    
+    cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+    int blockSize = 256;
+    int gridSize = (n + blockSize - 1) / blockSize;
+
+    vecDotProduct<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+
+    cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+
+    double sum {};
+    for (const float i : h_c) {
+        sum += i;
+    }
+
+    printf("Dot product is: %f\n", sum);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
